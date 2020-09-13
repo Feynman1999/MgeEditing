629fded62feec724deb69b2bcab08895b2720bab
aimax chenyuxiang <763972750@qq.com>
2020-09-13 22:00:58 +0800

    add mucan v2 for aistudio

--- configs/restorers/MUCAN/mucan_v2.py
+++ configs/restorers/MUCAN/mucan_v2.py
@@ -1,4 +1,4 @@
-exp_name = 'rbpn_x4_mge'
+exp_name = 'mucan_v2'
 
 scale = 4
 frames = 7
@@ -7,10 +7,8 @@ frames = 7
 model = dict(
     type='ManytoOneRestorer',
     generator=dict(
-        type='RBPN',
-        cl=128,
-        cm=128,
-        ch=32,
+        type='MUCAN',
+        ch=32*8,
         nframes = frames,
         input_nc = 3,
         output_nc = 3,
@@ -19,7 +17,7 @@ model = dict(
 
 # model training and testing settings
 train_cfg = None
-eval_cfg = dict(metrics=['PSNR'], crop_border=0)
+eval_cfg = dict(metrics=['PSNR'], crop_border=0, padding_multi = 4)
 img_norm_cfg = dict(mean=[0.5, 0.5, 0.5], std=[1, 1, 1])
 
 # dataset settings
@@ -28,8 +26,8 @@ eval_dataset_type = 'SRManyToOneDataset'
 test_dataset_type = 'SRManyToOneDataset'
 
 train_pipeline = [
-    dict(type='GenerateFrameIndices', interval_list=[1, 2], many2many = False),
-    dict(type='TemporalReverse', keys=['lq_path', 'gt_path'], reverse_ratio=0.2),
+    dict(type='GenerateFrameIndices', interval_list=[1, 2], many2many = False, name_padding = True),
+    dict(type='TemporalReverse', keys=['lq_path', 'gt_path'], reverse_ratio=0.3),
     dict(
         type='LoadImageFromFileList',
         io_backend='disk',
@@ -42,7 +40,7 @@ train_pipeline = [
         flag='unchanged'),
     dict(type='PairedRandomCrop', gt_patch_size=256),
     dict(type='RescaleToZeroOne', keys=['lq', 'gt']),
-    dict(type='Normalize', keys=['lq', 'gt'], to_rgb=False, **img_norm_cfg),
+    dict(type='Normalize', keys=['lq', 'gt'], to_rgb=True, **img_norm_cfg),
     dict(type='Flip', keys=['lq', 'gt'], flip_ratio=0.5, direction='horizontal'),
     dict(type='Flip', keys=['lq', 'gt'], flip_ratio=0.5, direction='vertical'),
     dict(type='RandomTransposeHW', keys=['lq', 'gt'], transpose_ratio=0.5),
@@ -51,7 +49,7 @@ train_pipeline = [
 ]
 
 eval_pipeline = [
-    dict(type="GenerateFrameIndiceswithPadding", padding='reflection_circle'),
+    dict(type="GenerateFrameIndiceswithPadding", padding='reflection_circle', name_padding = True),
     dict(
         type='LoadImageFromFileList',
         io_backend='disk',
@@ -63,13 +61,13 @@ eval_pipeline = [
         key='gt',
         flag='unchanged'),
     dict(type='RescaleToZeroOne', keys=['lq', 'gt']),
-    dict(type='Normalize', keys=['lq', 'gt'], to_rgb=False, **img_norm_cfg),
+    dict(type='Normalize', keys=['lq', 'gt'], to_rgb=True, **img_norm_cfg),
     dict(type='FramesToTensor', keys=['lq', 'gt']), # HWC -> CHW
     dict(type='Collect', keys=['lq', 'gt'])
 ]
 
 test_pipeline = [
-    dict(type="GenerateFrameIndiceswithPadding", padding='reflection_circle'),
+    dict(type="GenerateFrameIndiceswithPadding", padding='reflection_circle', name_padding = True),
     dict(
         type='LoadImageFromFileList',
         io_backend='disk',
@@ -88,14 +86,14 @@ eval_part = ("26", )
 data = dict(
     # train
     samples_per_gpu=3,
-    workers_per_gpu=2,
+    workers_per_gpu=3,
     train=dict(
         type='RepeatDataset',
         times=repeat_times,
         dataset=dict(
             type=train_dataset_type,
-            lq_folder= dataroot + "/mge/pngs/LR",
-            gt_folder= dataroot + "/mge/pngs/HR",
+            lq_folder= dataroot + "/mge/train/LR",
+            gt_folder= dataroot + "/mge/train/HR",
             num_input_frames=frames,
             pipeline=train_pipeline,
             scale=scale,
@@ -105,8 +103,8 @@ data = dict(
     eval_workers_per_gpu=4,
     eval=dict(
         type=eval_dataset_type,
-        lq_folder= dataroot + "/mge/pngs/LR",
-        gt_folder= dataroot + "/mge/pngs/HR",
+        lq_folder= dataroot + "/mge/train/LR",
+        gt_folder= dataroot + "/mge/train/HR",
         num_input_frames = frames,
         pipeline=eval_pipeline,
         scale=scale,
@@ -118,35 +116,36 @@ data = dict(
     test=dict(
         type=test_dataset_type,
         lq_folder= dataroot + "/Vid4/test/A",
+        num_input_frames = frames,
         pipeline=test_pipeline,
         scale=scale,
         mode="test")
 )
 
 # optimizer
-optimizers = dict(generator=dict(type='Adam', lr=5e-5, betas=(0.9, 0.999)))
+optimizers = dict(generator=dict(type='Adam', lr=3/16 * 1e-4, betas=(0.9, 0.999)))
 
 # learning policy
-total_epochs = 70 // repeat_times
+total_epochs = 100 // repeat_times
 
 # hooks
 lr_config = dict(policy='Step', step=[total_epochs // 10], gamma=0.7)
-checkpoint_config = dict(interval=total_epochs // 10)
+checkpoint_config = dict(interval=total_epochs // 50)
 log_config = dict(
-    interval=300,
+    interval=3,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='VisualDLLoggerHook')
     ])
 visual_config = None
-evaluation = dict(interval=30000, save_image=True)
+evaluation = dict(interval=30, save_image=True)
 
 # runtime settings
 work_dir = f'./workdirs/{exp_name}'
 load_from = None
 resume_from = None
 resume_optim = True
-workflow = [('train', 1)]
+workflow = 'train'
 
 # logger
 log_level = 'INFO'
